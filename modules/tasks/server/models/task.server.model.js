d457c1bdbe630730d55d9132aa12327378761108
CannedFish <lianggy0719@126.com>
2016-03-25 08:55:24 +0000

    implement task

---
+++ modules/tasks/server/models/task.server.model.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+/**
+ * Task Pluses
+ */
+var TaskPluses = new Schema({
+  description: {
+    type: String,
+    required: 'Please fill in a description'
+  },
+  value: {
+    type: Number,
+    required: 'Please fill in a value of this task'
+  }
+});
+
+/**
+ * Task Schema
+ */
+var TaskSchema = new Schema({
+  taskName: {
+    type: String,
+    trim: true,
+    required: 'Please fill in a taskname'
+  },
+  projectBelongs: {
+    type: String,
+    trim: true,
+    default: '--'
+  },
+  description: {
+    type: String,
+    required: 'Please fill in a description'
+  },
+  value: {
+    type: Number,
+    required: 'Please fill in a value of this task'
+  },
+  pluses: [TaskPluses],
+  taskType: {
+    type: String,
+    trim: true,
+    enum: ['code', 'document'],
+    default: 'code'
+  },
+  status: {
+    type: String,
+    trim: true,
+    enum: ['Not started', 'In progress', 'Pause', 'Completed', 'Terminated'],
+    default: 'Not started'
+  }
+});
+
