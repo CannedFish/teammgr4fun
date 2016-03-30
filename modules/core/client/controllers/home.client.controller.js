9255e29d56928576e331c03b5f9b857087391778
CannedFish <lianggy0719@126.com>
2016-03-30 10:06:22 +0000

    Implemented a simple page

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,8 +1,38 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-  function ($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', '$http', 'Authentication',
+  function ($scope, $http, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+
+    $http.get('/api/tasks').success(function(response) {
+      $scope.tasks = response;
+    });
+
+    $scope.createTask = function() {
+      var data = {
+        taskName: $scope.taskName,
+        createdBy: $scope.createdBy,
+        projectBelongs: $scope.project,
+        description: $scope.description,
+        value: $scope.value,
+        pluses: $scope.pluses,
+        taskType: $scope.taskType,
+        status: $scope.status
+      };
+      $http.post('/api/task/create', data).success(function(response) {
+        $scope.tasks.push(response);
+        $scope.taskName = '';
+        $scope.createdBy = '';
+        $scope.project = '';
+        $scope.description = '';
+        $scope.value = '';
+        $scope.pluses = '';
+        $scope.taskType = '';
+        $scope.status = '';
+      });
+    };
+
+    $scope.edit = true;
   }
 ]);
